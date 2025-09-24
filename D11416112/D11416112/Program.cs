a3c0bdabb6011b901fae36a2d7e41427dd3149bb
LINCHENGCHI <d11416112@gs.takming.edu.tw>
2025-09-24 10:50:15 +0800

    update

--- D11416112/D11416112/Program.cs
+++ D11416112/D11416112/Program.cs
@@ -6,7 +6,7 @@ namespace D11416112
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("輸入一個數");
+            Console.WriteLine("輸入一個");
             int num = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(num);
         }
